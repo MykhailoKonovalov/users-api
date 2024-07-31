6fc6a4e642019e01c606e9f46be477c7aef469b3
misha <myhajlo.konovalov@gmail.com>
2024-07-31 23:26:48 +0300

    Add User Fixture

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Random\RandomException;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @throws RandomException
+     */
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+
+            $user
+                ->setLogin('login_' . $i)
+                ->setPhone(mt_rand(10000000, 99999999))
+                ->setPass(substr(bin2hex(random_bytes(8)), 0, 8));
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
